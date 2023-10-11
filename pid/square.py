205849b080e748af2729ca393a80db924bcd2f52
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2023-10-11 10:51:26 +0200

    add square.py

---
+++ pid/square.py
@@ -0,0 +1,16 @@
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+import matplotlib.pyplot as plt
+
+x = [0, 0, 0.1, 0.1]
+y = [0, 0.1, 0.1, 0]
+
+fig, ax1 = plt.subplots()
+
+ax1.fill(x, y)
+
+ax1.set_xlim(0, 2)
+ax1.set_ylim(0, 2)
+
+plt.show()

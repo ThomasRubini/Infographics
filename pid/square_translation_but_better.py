874396fd6cd2b559cabd9d076f3eedcf2e855f52
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2023-10-18 14:11:11 +0200

    changed some settings
    Co-authored-by: Romain CLEMENT <rc2301@orange.fr>
    Co-authored-by: Audric V. <AudricV@users.noreply.github.com>

--- pid/square_translation_but_better.py
+++ pid/square_translation_but_better.py
@@ -6,21 +6,23 @@ from matplotlib.animation import FuncAnimation
 
 import numpy as np
 
-x = np.array([0, 0  , 0.2, 0.2])
-y = np.array([0, 0.2, 0.2, 0  ])
-z = np.array([1, 1  , 1  , 1  ])
+
+x = np.array([0,     0, 0.1, 0.1]).astype(float)
+y = np.array([0, 0.1, 0.1,   0]).astype(float)
+z = np.array([1,   1,   1,   1]).astype(float)
 n = len(x)
 
-Tx = -0.1
-Ty = 0.3
+Tx = 0.0008
+Ty = 0.001
 Tm = np.array([[1, 0, Tx],
                [0, 1, Ty],
                [0, 0, 1 ]])
 
 fig, ax1 = plt.subplots()
 
-ax1.set_xlim(-10, 10)
-ax1.set_ylim(-10, 10)
+# bounding box zoom
+ax1.set_xlim(-1, 1)
+ax1.set_ylim(-1, 1)
 
 def update(frame):
     print("New frame", "="*80)
@@ -29,6 +31,11 @@ def update(frame):
     print(x, y, z)
     return ax1.fill(x, y, color="blue")
 
-ani = FuncAnimation(fig, update, frames=np.linspace(0, 2, 1000), interval=1000, blit=True)
+# nombre d'itération
+frames = np.linspace(0, 1, 10000)
+
+# attente entre chaque itération
+interval = 0.01
+ani = FuncAnimation(fig, update, frames=frames, interval=interval, blit=True)
 
 plt.show()

f25ca39547afc591fb7da0f0d1c26fe1c30e304a
Romain CLEMENT <rc2301@orange.fr>
2023-10-25 17:20:34 +0200

    Implemented repeat feature It allow to redo each movement again and again

--- pid/polygon.py
+++ pid/polygon.py
@@ -11,28 +11,40 @@ class Polygon:
     counter: decreasing counter of frames that are need to be spent with the current matrix
     """
 
-    def __init__(self, x: np.ndarray, y: np.ndarray, matrixes: list[tuple[np.ndarray, int]], color: str="blue"):
+    def __init__(self, x: np.ndarray, y: np.ndarray,
+                 matrixes: list[tuple[np.ndarray, int]],
+                 color: str="blue", repeat: bool=False):
         self.n = len(x)
         assert len(y) == self.n
         self.x = x.astype(float)
         self.y = y.astype(float)
         self.matrixes = matrixes
         self.color = color
+        self.repeat = repeat
 
         self.__pop_matrix()
+    
+    def __reuse_matrix(self):
+        """
+        Append the current matrix at the end of the animation queue to reuse it later
+        """
+        self.matrixes.append((self.current_matrix, self.init_counter))
     
     def __pop_matrix(self):
         """
         Pops a new current matrix from self.matrixes, if possible
-        if no matrixes are present in the list, the function will raise an error
+        if no matrixes are present in the list, the current matrix becomes None
         """
         if len(self.matrixes) == 0:
-            raise Exception(f"asking to change current matrix when matrix list is empty for polygon {self}")
-        self.current_matrix = self.matrixes[0][0]
-        self.counter = self.matrixes[0][1]
-        self.matrixes = self.matrixes[1:]
+            self.current_matrix = None
+            return
+        self.current_matrix, self.init_counter = self.matrixes.pop(0)
+        self.counter = self.init_counter
 
     def update(self):
+        if self.current_matrix is None:
+            return
+        
         for point_i in range(self.n):
             self.x[point_i], self.y[point_i], z = self.current_matrix @ np.array([self.x[point_i], self.y[point_i], 1])
             assert z == 1
@@ -41,7 +53,12 @@ class Polygon:
         # Typicailly this is used for the last matrix in the list
         if self.counter != None:
             self.counter -= 1
+            # If the end of the matrix is reached
             if self.counter == 0:
+                # Append the current matrix if looping
+                if self.repeat:
+                    self.__reuse_matrix()
+                # Remove the current matrix
                 self.__pop_matrix()
     
     def __repr__(self) -> str:
